87a52d6a5fb4b6bb2c71d99a8726f1a0a8e97690
1996tianwen <1996tianwen@gmail.com>
2018-05-08 23:15:57 +0800

    update

--- src/main/java/com/wtw/bpc/service/ChainService.java
+++ src/main/java/com/wtw/bpc/service/ChainService.java
@@ -1,4 +1,34 @@
 package com.wtw.bpc.service;
 
+
+import com.sun.corba.se.impl.orb.ORBConfiguratorImpl;
+import org.apache.commons.chain.Catalog;
+import org.apache.commons.chain.CatalogFactory;
+import org.apache.commons.chain.Command;
+import org.apache.commons.chain.Context;
+import org.apache.commons.chain.config.ConfigParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ */
 public class ChainService {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Catalog catalog;
+    private Command command;
+
+    public void loadCatalogFile(String cfgFile) throws Exception {
+        ConfigParser parser = new ConfigParser();
+        parser.parse(ChainService.class.getClassLoader().getResource(cfgFile));
+    }
+
+    public void execute(String name, Context context) throws Exception {
+        String[] nameAttr = name.split(":");
+        String catalogName = nameAttr[0];
+        String commandName = nameAttr[1];
+        catalog = CatalogFactory.getInstance().getCatalog(catalogName);
+        command = catalog.getCommand(commandName);
+        command.execute(context);
+    }
 }
